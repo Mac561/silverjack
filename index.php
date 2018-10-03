fdc46f07b5794974b964579fe1e81439c643eb6d
Ivonne Garcia Santos <ivonnes118@gmail.com>
2018-10-03 21:26:46 +0000

    connecting functions file

--- index.php
+++ index.php
@@ -8,24 +8,14 @@ session_start();//starting with session for time
 <!DOCTYPE html>
 <html>
     <head>
-<<<<<<< HEAD
-        <title> Silver Jack </title>
-    </head>
-    <body>
- <div>
-     <h1> Horror Jack!!! </h1>
- </div>
-<<<<<<< HEAD
-
-=======
->>>>>>> 14ed8d42a1e7bff7dae59a0dc87c186b3d0e37b5
-=======
         <title>SilverJack</title>
         <link href="css/style.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        
+         <h1>Silverjack</h1>
+        <?php
+         include 'functions.php'
+        ?>
 
->>>>>>> f199fb24137a91b2bb47cb8f2680f9a2b9a1a903
     </body>
 </html>
